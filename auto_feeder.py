34f4debc819b8f8157c8b3f76d3841ae6e173b45
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-02-04 21:03:12 +0000

    Added comments to auto feeder.

--- auto_feeder.py
+++ auto_feeder.py
@@ -5,11 +5,14 @@ from Servo import feeding_time
 
 times = []
 
+#Updates the times list with times in the auto feed list
 def update_times():
+    times.clear()
     time_file = open("auto_feed_times.txt")
     for time in time_file.readlines():
         times.append(lit_eval(time.strip()))
 
+#Checks the current time against the list of set times every second
 def start_check():
     update_times()
     while True:
