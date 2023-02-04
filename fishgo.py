f331daaf0cce220affd65d8010190f53433a4340
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-02-04 14:37:39 +0000

    Removed custom bot message which caused errors.

--- fishgo.py
+++ fishgo.py
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix=["\\","~"],intents = intents)
 @bot.command(name="feed",help = "moves the servo to feed the fish")
 async def feedfish(ctx, freq):
     print(freq)
-    await ctx.channel.send(content = str("feeding time has begun, feeding ",freq," times!"))
+    await ctx.channel.send("feeding time has begun, feeding")
     Servo.feeding_time(int(freq))
     await ctx.channel.send("feeding time has come to an end")
 ##command to turn on the lights
