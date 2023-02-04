6ed06520ac93bbf795981799cc96f798039aa449
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-02-04 20:02:53 +0000

    Fixed video capture and created a method to record a given amount of time.

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 #cap = cv2.VideoCapture('https://10.2.177.147:8080/%27)
 import cv2
 import pathlib
+import time
 
 # URL of the IP webcam
 url = "http://10.2.177.147:8080/video"
@@ -8,24 +9,36 @@ url = "http://10.2.177.147:8080/video"
 # Load the video from the URL
 cap = cv2.VideoCapture(url)
 
-vid = cv2.VideoWriter(pathlib.Path().resolve(),"/test_vid.mp4")
+#Captures video for the given amount of seconds
+def record(duration):
+    #Grabs one frame from the video to calibrate the capture size
+    (grabbed, frame) = cap.read()
+    fshape = frame.shape
+    fheight = fshape[0]
+    fwidth = fshape[1]
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-while True:
-    # Read the next frame from the video stream
-    ret, frame = cap.read()
-    # Check if the frame was successfully read
-    if ret:
-        vid.write(frame)
-    else:
-        # If there was an error reading the frame, print an error message
-        print("Error: Could not read frame from the video stream.")
+    #Formats the video writer
+    vid = cv2.VideoWriter('output.avi',fourcc, 20.0, (fwidth,fheight))
 
-    # Check if the user pressed the 'q' key
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    start_time = time.time()
 
-# Release the video capture object
-cap.release()
-vid.release()
-# Close all the windows
-cv2.destroyAllWindows()
+    while True:
+        # Read the next frame from the video stream
+        ret, frame = cap.read()
+        # Check if the frame was successfully read
+        if ret:
+            vid.write(frame)
+        else:
+            # If there was an error reading the frame, print an error message
+            print("Error: Could not read frame from the video stream.")
+        if time.time()-start_time > duration:
+            break
+        
+
+    # Release the video capture object
+    vid.release()
+    cap.release()
+
+    # Close all the windows
+    cv2.destroyAllWindows()
