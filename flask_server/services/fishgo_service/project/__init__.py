6f4c4627d75a705996d89f5553ed070f8da76797
Darshan-Kavalakatt <75749929+Darshan-Kavalakatt@users.noreply.github.com>
2023-02-04 20:07:12 +0000

    added opencv
    opencv added

--- flask_server/services/fishgo_service/project/__init__.py
+++ flask_server/services/fishgo_service/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify,Response
 import project.util as util
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
@@ -6,3 +6,15 @@ app.config.from_object("project.config.Config")
 @app.route("/flashbang",methods= ['GET'])
 def flashbang():
     util.flash_bang()
+    return jsonify(success = True)
+
+@app.route("/video",methods =['GET'])
+def video():
+    util.videoCap()
+    def generate():
+        with open("output.mp4", "rb") as f:
+            data = f.read(1024)
+            while data:
+                yield data
+                data = f.read(1024)
+    return Response(generate(), content_type="video/mp4")
