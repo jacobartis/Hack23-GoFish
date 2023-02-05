74f737bf5f7dad91a848cefe7e8487424f9cd317
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-02-05 01:23:00 +0000

    Fixed add and delete time commands, delete command currently displays error message after successfuly executing. Created a second thread to run the auto feeder code on.

--- fishgo.py
+++ fishgo.py
@@ -2,10 +2,15 @@ import discord
 from discord.utils import get
 from discord.ext import commands
 from ast import literal_eval
+import threading
 import Servo
 import auto_feeder
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=["\\","~"],intents = intents)
+
+#Creates a thread to run the auto feeder in parallel
+auto_feeder_thread = threading.Thread(target=auto_feeder.start_check)
+auto_feeder_thread.start()
 
 ##command to move servo
 @commands.has_role("moderator")
@@ -21,6 +26,7 @@ async def feedfish(ctx, freq):
         await ctx.channel.send("feeding time has come to an end")
     except:
         await ctx.channel.send("incorrect feed command, please put in form '~feed value' where value is a number or blank")
+
 #Handles the user not inputing a message
 @feedfish.error
 async def feedfisherror(ctx, error):
@@ -28,7 +34,7 @@ async def feedfisherror(ctx, error):
     Servo.feeding_time(5)
     await ctx.channel.send("feeding time has come to an end")
 
-##command to turn on the lights
+#command to turn on the lights
 @commands.has_role("moderator")
 @bot.command(name="light",help = "toggles the lights")
 async def flashbang(ctx):
@@ -45,33 +51,34 @@ async def add_feed_time(ctx, time:str):
     try:
         #Checks the input are correct
         assert(len(time.split(","))==3)
-        assert(int(time.split(",")[0])<24 and int(time.split(",")[0])>0)
-        assert(int(time.split(",")[1])<60 and int(time.split(",")[1])>0)
-        assert(int(time.split(",")[2])<60 and int(time.split(",")[2])>0)
+        assert(int(time.split(",")[0])<24 and int(time.split(",")[0])>=0)
+        assert(int(time.split(",")[1])<60 and int(time.split(",")[1])>=0)
+        assert(int(time.split(",")[2])<60 and int(time.split(",")[2])>=0)
 
         #Opens the auto feed file, formats the input and write to file
-        feed_times = open("auto_feed_times.txt","a")
+        
         new_time = []
         for x in time.split(","):
             new_time.append(int(x))
         
-        for x in feed_times.readlines():
-            if new_time == literal_eval(x):
-                ctx.channel.send(f"{new_time} is already present on the auto feed schedule!")
-                return
+        with open("auto_feed_times.txt","r") as feed_times:
+            for x in feed_times.readlines():
+                if new_time == literal_eval(x.strip()):
+                    await ctx.channel.send(f"{new_time} is already present on the auto feed schedule!")
+                    return
         
-        feed_times.write("\n"+str(new_time))
-        feed_times.close()
-        await ctx.channel.send(f"added {new_time} to auto feed")
+        with  open("auto_feed_times.txt","a") as feed_times:
+            feed_times.write("\n"+str(new_time))
 
-        auto_feeder.update_times()
+        await ctx.channel.send(f"added {new_time} to auto feed")
+    
     except:
         await ctx.channel.send("error! please put in form 'add_feed_time hour,minute,second'")
 
 ##command to print all auto fed times
 @commands.has_role("moderator")
 @bot.command(name="feed_times",help = "shows all auto feed times")
-async def add_feed_time(ctx):
+async def feed_times(ctx):
     await ctx.channel.send("current auto feed times:")
     for x in open("auto_feed_times.txt").readlines():
         if x.strip() != "":
@@ -80,13 +87,13 @@ async def add_feed_time(ctx):
 ##command to print all auto fed times
 @commands.has_role("moderator")
 @bot.command(name="delete_feed_time",help = "deletes a time on the auto feed timetable")
-async def add_feed_time(ctx, time:str):
+async def delete_feed_time(ctx, time:str):
     try:
         #Checks the input are correct
         assert(len(time.split(","))==3)
-        assert(int(time.split(",")[0])<24 and int(time.split(",")[0])>0)
-        assert(int(time.split(",")[1])<60 and int(time.split(",")[1])>0)
-        assert(int(time.split(",")[2])<60 and int(time.split(",")[2])>0)
+        assert(int(time.split(",")[0])<24 and int(time.split(",")[0])>=0)
+        assert(int(time.split(",")[1])<60 and int(time.split(",")[1])>=0)
+        assert(int(time.split(",")[2])<60 and int(time.split(",")[2])>=0)
 
         #Opens the auto feed file, formats the input and write to file
         
@@ -103,19 +110,17 @@ async def add_feed_time(ctx, time:str):
         del_val = 0
         with open("auto_feed_times.txt","w") as feed_times:
             for line in lines:
-                if new_time != literal_eval(line):
+                if new_time != literal_eval(line.strip()):
                     feed_times.write(line)
                 else:
                     del_val += 1
         
-        auto_feeder.update_times()
-
         #Checks if there was a valid value to delete
         if del_val==0:
-            ctx.channel.send(f"error! {time} is not pressent on the timetable")
+            await ctx.channel.send(f"error! {time} is not pressent on the timetable")
             return
         
-        ctx.channel.send(f"{time} removed from the auto feed timetable")
+        await ctx.channel.send(f"{time} removed from the auto feed timetable")
 
     except:
         await ctx.channel.send("error! please put in form 'delete_feed_time hour,minute,second'")
@@ -127,4 +132,3 @@ token_f = open("token.txt","r")
 bot.run(token_f.readline())
 token_f.close()
 
-auto_feeder.start_check()
